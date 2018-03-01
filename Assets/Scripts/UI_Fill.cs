1bcebc1278d9dcf9b4f0fe2c9a0f1a3f119de570
gkdms2749 <gkdms2749100@gmail.com>
2018-03-01 21:07:54 +0900

    GameScene UI배치
    시간은 우선 100초이고 PLAY누르면 시작되게 수정해야함. 시간초text  00:00:00부분도 굳이 1시간이상짜리를 만들필요없어보임. 교수님께 00:00으로 해도 되는지 100초정도면 괜찮은지 확인해야함.

---
+++ Assets/Scripts/UI_Fill.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_Fill : MonoBehaviour {
+	
+	[SerializeField]
+	private Image fill;
+
+	[SerializeField]
+	private float fillAmount =1;
+
+	[SerializeField]
+	private GameObject button;
+	[SerializeField]
+	private GameObject GameStart;
+	[SerializeField]
+	private GameObject GameUI;
+
+	[SerializeField]
+	private Text timerText;
+
+	// Use this for initialization
+	void Start () {
+		GameUI.SetActive (true);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (fillAmount >= 0) {
+			fillAmount = 1 - Time.time / 100;
+			UpdateBar ();
+			Debug.Log (fillAmount);
+
+			string minutes = ((int) (fillAmount*100)/60).ToString();
+			string seconds = ((int)(fillAmount*100-60)).ToString();
+
+			timerText.text = "00:0" + minutes + ":" + seconds;
+		}
+		else
+			button.SetActive (true);
+	}
+
+
+	private void UpdateBar(){
+		if(fillAmount != fill.fillAmount)
+			fill.fillAmount = fillAmount;
+	}
+}
